34ea971258c5f20f0c32131da041344435504419
jon-davis <jonathondevindavis@gmail.com>
2017-11-03 16:46:53 -0700

    initial subtraction implemented

--- src/main.rs
+++ src/main.rs
@@ -1,25 +1,14 @@
 mod number;
+//mod number_test;
 use number::*;
 
 fn main() {
-    let num1 = Number::new(11);
-    let num2 = Number::new(9223372036854775807);
-    let num3 = Number::new(9223372036854775807);
-    let num4 = &num1 + &num2;
+    let num1 = Number::new(10);
+    let num2 = Number::new(20);
+    let sub = &num1 - &num2;
 
-    println!("{:X} + {:X} =  {:X}",&num1,&num2,&num4);
-    println!("{:X} == {:X} => {}",&num1,&num2,&num1 == &num2);
-    println!("{:X} != {:X} => {}",&num1,&num2,&num1 != &num2);
-    println!("{:X} == {:X} => {}",&num2,&num3,&num2 == &num3);
-    println!("{:X} != {:X} => {}",&num2,&num3,&num2 != &num3);
-    println!("{:X} < {:X} => {}",&num1,&num2,&num1 < &num2);
-    println!("{:X} > {:X} => {}",&num1,&num2,&num1 > &num2);
-    println!("{:X} < {:X} => {}",&num2,&num3,&num2 < &num4);
-    println!("{:X} <= {:X} => {}",&num2,&num3,&num2 <= &num4);
-    println!("{:X} > {:X} => {}",&num2,&num3,&num2 > &num4);
-    println!("{:X} >= {:X} => {}",&num2,&num3,&num2 >= &num4);
-    println!("{:X} >= {:X} => {}",&num1,&num1,&num1 >= &num1);
-    println!("{:X} <= {:X} => {}",&num1,&num1,&num1 <= &num1);
-    println!("{:X} > {:X} => {}",&num1,&num1,&num1 > &num1);
-    println!("{:X} < {:X} => {}",&num1,&num1,&num1 < &num1);
+    println!("{:X} > {:X} = {}",&num1,&sub,&num1 > &sub);
+    println!("{:X} < {:X} = {}",&num1,&sub,&num1 < &sub);
+    println!("{:X} > {:X} = {}",&num1,&num2,&num1 > &num2);
+    println!("{:X} < {:X} = {}",&num1,&num2,&num1 < &num2);
 }
--- src/number.rs
+++ src/number.rs
@@ -1,16 +1,23 @@
-use std::ops::{Add, Sub, Div, Mul, Rem};
+use std::ops::{Add, Sub, Div, Mul, Rem, Neg};
 use std::cmp::{PartialEq,PartialOrd,Ordering};
 use std::fmt;
 
 pub struct Number {
     value : Vec<u64>,
-    negative : bool,
+    negative: bool,
 }
 
 impl Number {
     pub fn new(init : u64) -> Number {
         Number {
             value: vec![init],
+            negative: false,
+        }
+    }
+
+    pub fn abs(&self) -> Number {
+        Number {
+            value: self.value.clone(),
             negative: false,
         }
     }
@@ -24,30 +31,55 @@ impl<'a,'b> Add<&'b Number> for &'a Number {
         let capacity = self.value.len().max(num.value.len()) + 1;
         let mut new_value = Vec::with_capacity(capacity);
         let mut carry = 0;
-        let mut negative = false;
-        for i in self.value.iter().zip(num.value.iter()){
-            let (a,b) = i;
-            if self.negative == num.negative {
-                let new_num = a+b+carry;
-                carry = if &new_num < a || &new_num < b {1} else {0};
+        let (min,max) = if &self.abs() < &num.abs() {(self,num)} else {(num,self)};
+        for i in min.value.iter().zip(max.value.iter()){
+            let (a,b) = (i.0.clone(),i.1.clone());
+            if min.negative == max.negative {
+                let (new_num,overflow1) = a.overflowing_add(b);
+                let (new_num,overflow2) = new_num.overflowing_add(carry);
+                carry = if overflow1 || overflow2 {1} else {0};
                 new_value.push(new_num);
-                negative = self.negative;
             } else {
-                //TODO implement subtraction
+                let (a,carry) = if b < (a + carry) {(18446744073709551615-a,1)} else {(a,0)};
+                let new_num = b-a-carry;
+                new_value.push(new_num);
             }
         }
-
+        if carry > 0 && max.negative == min.negative {
+            new_value.push(carry);
+        }
         Number {
             value: new_value,
-            negative: negative,
+            negative: max.negative,
         }
     }
+}
 
+impl<'a> Neg for &'a Number {
+    type Output = Number;
+
+    fn neg(self) -> Number {
+        Number{
+            value: self.value.clone(),
+            negative: !self.negative,
+        }
+    }
+}
+
+impl<'a,'b> Sub<&'b Number> for &'a Number {
+    type Output = Number;
+
+    fn sub(self, num : &'b Number) -> Number {
+        self + &(-num)
+    }
 }
 
 impl PartialEq for Number {
 
     fn eq(&self, num : &Number) -> bool{
+        if self.negative != num.negative {
+            return false;
+        }
         for i in self.value.iter().zip(num.value.iter()){
             let (a,b) = i;
             if a != b {return false};
@@ -57,6 +89,9 @@ impl PartialEq for Number {
 
     fn ne(&self, num : &Number) -> bool{
         for i in self.value.iter().zip(num.value.iter()){
+            if self.negative != num.negative {
+                return true;
+            }
             let (a,b) = i;
             if a == b {return false};
         }
@@ -76,6 +111,12 @@ impl PartialOrd for Number {
     }
 
     fn lt(&self, num : &Number) -> bool {
+        if num.negative && !self.negative {
+            return false;
+        } else if num.negative != self.negative {
+            return true;
+        }
+
         for i in self.value.iter().rev().zip(num.value.iter().rev()){
             let (a,b) = i;
             if a < b {return true}
@@ -83,7 +124,13 @@ impl PartialOrd for Number {
         }
         return false;
     }
+
     fn gt(&self, num : &Number) -> bool {
+        if num.negative && !self.negative {
+            return true;
+        } else if num.negative != self.negative {
+            return false;
+        }
         for i in self.value.iter().rev().zip(num.value.iter().rev()){
             let (a,b) = i;
             if a < b {return false}
@@ -99,13 +146,6 @@ impl PartialOrd for Number {
     }
 }
 /*
-impl<'a,'b> Sub<&'b Number> for &'a Number {
-    type Output = Number;
-
-    fn sub(self, num :&'b Number) -> Number {
-        Number {num: self.num - num.num}
-    }
-}
 
 impl<'a,'b> Mul<&'b Number> for &'a Number {
     type Output = Number;
@@ -147,14 +187,18 @@ impl fmt::Binary for Number {
 impl fmt::UpperHex for Number {
     fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
         let mut string_value = String::with_capacity(self.value.len()*16);
-        let mut fmt_string = String::with_capacity(self.value.len()*24);
+        let mut fmt_string = String::with_capacity(self.value.len()*24+1);
 
         //convert the number into Hex
         for i in self.value.iter().rev() {
-            string_value.push_str(&format!("{:X}",i));
+            string_value.push_str(&format!("{:016X}",i));
         }
 
         //space out the numbers
+        if self.negative  {
+            fmt_string.push('-');
+        }
+
         for (i,c) in string_value.chars().enumerate(){
             fmt_string.push(c);
             if i % 2 != 0 {
